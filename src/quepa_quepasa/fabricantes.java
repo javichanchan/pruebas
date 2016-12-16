98424c7d1c855a27356818a04e73189a814e1e4c
javichanchan <somgokuxibicomam@hotmail.com>
2016-12-16 08:18:28 +0100

    Update fabricantes.java

--- src/quepa_quepasa/fabricantes.java
+++ src/quepa_quepasa/fabricantes.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package quepa_quepasa; //esta aplicacion es super chunga
+package quepa_quepasa; //esta aplicacion es super chunga, pero  chunga chunga
 
 import java.sql.Connection;
 import java.sql.DriverManager;
